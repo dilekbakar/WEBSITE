d6b121bc37b8885b32fa6045b8479252a74e42c2
dilekbakar <dilekbakar22@gmail.com>
2022-08-02 16:15:41 +0300

    first commit
    first commit

---
+++ MSProject/MSProject/Controllers/TakımlarController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MSProject.Controllers
+{
+    public class TakımlarController : Controller
+    {
+        // GET: Takım1
+        public ActionResult Takim1()
+        {
+            return View();
+        }
+        public ActionResult Takim2()
+        {
+            return View();
+        }
+        public ActionResult Takim3()
+        {
+            return View();
+        }
+    }
+}
